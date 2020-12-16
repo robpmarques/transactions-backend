06f8add4764cd5659c954d271d6bb9f325b6120a
Roberto Marques <matsufrg@gmail.com>
2020-12-15 22:21:09 -0300

    Criando funcionalidades de Models de Transaction e Category

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,26 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn()
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column()
   value: number;
 
+  @Column()
   category_id: string;
 
+  @Column()
   created_at: Date;
 
+  @Column()
   updated_at: Date;
 }
 
